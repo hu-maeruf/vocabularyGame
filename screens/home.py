40bd031a3d33a585a6ce744f65342808efce94a8
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2026-06-04 18:40:58 +0800

    feat: add home screen with play button

---
+++ screens/home.py
@@ -0,0 +1,22 @@
+import pygame
+
+def init():
+    button_rect = pygame.Rect(0, 0, 200, 70)
+    button_rect.center = (400, 300)
+    text_font = pygame.font.Font("assets/fonts/Pixeltype.ttf", 50)
+    text_surface = text_font.render("Play", True, (0, 0, 255))
+    text_rect = text_surface.get_rect()
+    text_rect.center = button_rect.center
+    return button_rect, text_surface, text_rect
+
+def draw(screen, button_rect, text, text_rect):
+    pygame.draw.rect(screen, (0, 0, 0), button_rect)
+    screen.blit(text, text_rect)
+
+def handle_events(event, state, button_rect):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if button_rect.collidepoint(event.pos):
+            print("Button clicked!")
+            state = "category"
+
+    return state
