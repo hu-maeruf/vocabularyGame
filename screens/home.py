1f536dda67fe340070330715933f4f3918ce4b25
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2026-06-04 22:35:59 +0800

    fix: replace invalid font with Atkinson Hyperlegible

--- screens/home.py
+++ screens/home.py
@@ -3,8 +3,8 @@ import pygame
 def init():
     button_rect = pygame.Rect(0, 0, 200, 70)
     button_rect.center = (400, 300)
-    text_font = pygame.font.Font("assets/fonts/Pixeltype.ttf", 50)
-    text_surface = text_font.render("Play", True, (0, 0, 255))
+    text_font = pygame.font.Font("assets/fonts/AtkinsonHyperlegible-Regular.ttf", 50)
+    text_surface = text_font.render("Play", True, (255, 255, 255))
     text_rect = text_surface.get_rect()
     text_rect.center = button_rect.center
     return button_rect, text_surface, text_rect
@@ -18,5 +18,6 @@ def handle_events(event, state, button_rect):
         if button_rect.collidepoint(event.pos):
             print("Button clicked!")
             state = "category"
-
+        else:
+            state = "home"
     return state
