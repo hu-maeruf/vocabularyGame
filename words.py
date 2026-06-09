cfd3c3b6ac4489d8ba2483e9d2277699e07de351
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2026-06-09 15:20:24 +0800

    feat: add image paths to all words

--- words.py
+++ words.py
@@ -9,72 +9,75 @@ init_animals_easy = [
     {"name": "Rabbit", "hint": "I love eating carrots", "image": "assets/images/animals/rabbit.png"},
     {"name": "Frog", "hint": "I say ribbit", "image": "assets/images/animals/frog.png"},
     {"name": "Goat", "hint": "I say maa", "image": "assets/images/animals/goat.png"},
+    {"name": "Monkey", "hint": "I live in the forest", "image": "assets/images/animals/monkey.png"},
+    {"name": "Lion", "hint": "I am the king of the jungle", "image": "assets/images/animals/lion.png"},
+    {"name": "Tiger", "hint": "I am Tiger", "image": "assets/images/animals/tiger.png"},
 ]
 
 init_animals_diff = [
-    {"name": "Elephant", "hint": "I am the biggest animal"},
-    {"name": "Giraffe", "hint": "I have a very long neck"},
-    {"name": "Kangaroo", "hint": "I carry my baby in a pouch"},
-    {"name": "Crocodile", "hint": "I live in water and land"},
-    {"name": "Peacock", "hint": "I have colorful feathers"},
+    {"name": "Elephant", "hint": "I am the biggest animal", "image": "assets/images/animals/elephant.png"},
+    {"name": "Giraffe", "hint": "I have a very long neck", "image": "assets/images/animals/giraffe.png"},
+    {"name": "Kangaroo", "hint": "I carry my baby in a pouch", "image": "assets/images/animals/kangaroo.png"},
+    {"name": "Crocodile", "hint": "I live in water and land", "image": "assets/images/animals/crocodile.png"},
+    {"name": "Snake", "hint": "I am thinner than a human", "image": "assets/images/animals/snake.png"},
 ]
 
 init_fruits_easy = [
-    {"name": "Apple", "hint": "This is Apple"},
-    {"name": "Banana", "hint": "This is Banana"},
-    {"name": "Orange", "hint": "This is Orange"},
-    {"name": "Mango", "hint": "This is Mango"},
-    {"name": "Grapes", "hint": "This is Grapes"},
-    {"name": "Lemon", "hint": "This is Lemon"},
-    {"name": "Watermelon", "hint": "This is Watermelon"},
-    {"name": "Strawberry", "hint": "This is Strawberry"},
+    {"name": "Apple", "hint": "This is Apple", "image": "assets/images/frut_veg/apple.png"},
+    {"name": "Banana", "hint": "This is Banana", "image": "assets/images/frut_veg/banana.png"},
+    {"name": "Orange", "hint": "This is Orange", "image": "assets/images/frut_veg/orange.png"},
+    {"name": "Mango", "hint": "This is Mango", "image": "assets/images/frut_veg/mango.png"},
+    {"name": "Grapes", "hint": "This is Grapes", "image": "assets/images/frut_veg/grapes.png"},
+    {"name": "Lemon", "hint": "This is Lemon", "image": "assets/images/frut_veg/lemon.png"},
+    {"name": "Watermelon", "hint": "This is Watermelon", "image": "assets/images/frut_veg/watermelon.png"},
+    {"name": "Strawberry", "hint": "This is Strawberry", "image": "assets/images/frut_veg/strawberry.png"},
 ]
 
 init_fruits_diff = [
-    {"name": "Pineapple", "hint": "This is Pineapple"},
-    {"name": "Cherry", "hint": "This is Cherry"},
-    {"name": "Papaya", "hint": "This is Papaya"},
-    {"name": "Blueberry", "hint": "This is Blueberry"},
-    {"name": "Avocado", "hint": "This is Avocado"},
-    {"name": "Coconut", "hint": "This is Coconut"},
+    {"name": "Pineapple", "hint": "This is Pineapple", "image": "assets/images/frut_veg/pineapple.png"},
+    {"name": "Cherry", "hint": "This is Cherry", "image": "assets/images/frut_veg/cherry.png"},
+    {"name": "Papaya", "hint": "This is Papaya", "image": "assets/images/frut_veg/papaya.png"},
+    {"name": "Blueberry", "hint": "This is Blueberry", "image": "assets/images/frut_veg/blueberry.png"},
+    {"name": "Avocado", "hint": "This is Avocado", "image": "assets/images/frut_veg/avocado.png"},
+    {"name": "Coconut", "hint": "This is Coconut", "image": "assets/images/frut_veg/coconut.png"},
 ]
 
 init_veg_easy = [
-    {"name": "Carrot", "hint": "This is Carrot"},
-    {"name": "Corn", "hint": "This is Corn"},
-    {"name": "Potato", "hint": "This is Potato"},
-    {"name": "Tomato", "hint": "This is Tomato"},
-    {"name": "Onion", "hint": "This is Onion"},
-    {"name": "Cabbage", "hint": "This is Cabbage"},
-    {"name": "Pepper", "hint": "This is Pepper"},
+    {"name": "Carrot", "hint": "This is Carrot", "image": "assets/images/frut_veg/carrot.png"},
+    {"name": "Corn", "hint": "This is Corn", "image": "assets/images/frut_veg/corn.png"},
+    {"name": "Potato", "hint": "This is Potato", "image": "assets/images/frut_veg/potato.png"},
+    {"name": "Tomato", "hint": "This is Tomato", "image": "assets/images/frut_veg/tomato.png"},
+    {"name": "Onion", "hint": "This is Onion", "image": "assets/images/frut_veg/onion.png"},
+    {"name": "Cabbage", "hint": "This is Cabbage", "image": "assets/images/frut_veg/cabbage.png"},
+    {"name": "Pepper", "hint": "This is Pepper", "image": "assets/images/frut_veg/pepper.png"},
 ]
 
 init_veg_diff = [
-    {"name": "Broccoli", "hint": "This is Broccoli"},
-    {"name": "Cucumber", "hint": "This is Cucumber"},
-    {"name": "Pumpkin", "hint": "This is Pumpkin"},
-    {"name": "Ginger", "hint": "This is Ginger"},
+    {"name": "Broccoli", "hint": "This is Broccoli", "image": "assets/images/frut_veg/broccoli.png"},
+    {"name": "Cucumber", "hint": "This is Cucumber", "image": "assets/images/frut_veg/cucumber.png"},
+    {"name": "Pumpkin", "hint": "This is Pumpkin", "image": "assets/images/frut_veg/pumpkin.png"},
+    {"name": "Ginger", "hint": "This is Ginger", "image": "assets/images/frut_veg/ginger.png"},
 ]
 
 init_colors_easy = [
-    {"name": "Red", "hint": "This is Red"},
-    {"name": "Blue", "hint": "This is Blue"},
-    {"name": "Green", "hint": "This is Green"},
-    {"name": "Yellow", "hint": "This is Yellow"},
-    {"name": "Pink", "hint": "This is Pink"},
-    {"name": "Black", "hint": "This is Black"},
-    {"name": "White", "hint": "This is White"},
-    {"name": "Orange", "hint": "This is Orange"},
-    {"name": "Purple", "hint": "This is Purple"},
-    {"name": "Brown", "hint": "This is Brown"},
+    {"name": "Red", "hint": "This is Red", "image": "assets/images/colors/red.png"},
+    {"name": "Blue", "hint": "This is Blue", "image": "assets/images/colors/blue.png"},
+    {"name": "Green", "hint": "This is Green", "image": "assets/images/colors/green.png"},
+    {"name": "Yellow", "hint": "This is Yellow", "image": "assets/images/colors/yellow.png"},
+    {"name": "Pink", "hint": "This is Pink", "image": "assets/images/colors/pink.png"},
+    {"name": "Black", "hint": "This is Black", "image": "assets/images/colors/black.png"},
+    {"name": "White", "hint": "This is White", "image": "assets/images/colors/white.png"},
+    {"name": "Orange", "hint": "This is Orange", "image": "assets/images/colors/orange.png"},
+    {"name": "Purple", "hint": "This is Purple", "image": "assets/images/colors/purple.png"},
+    {"name": "Brown", "hint": "This is Brown", "image": "assets/images/colors/brown.png"},
 ]
 
 init_colors_diff = [
-    {"name": "Gray", "hint": "This is Gray"},
-    {"name": "Gold", "hint": "This is Gold"},
-    {"name": "Silver", "hint": "This is Silver"},
-    {"name": "Cyan", "hint": "This is Cyan"},
-    {"name": "Magenta", "hint": "This is Magenta"},
+    {"name": "Gray", "hint": "This is Gray", "image": "assets/images/colors/gray.png"},
+    {"name": "Gold", "hint": "This is Gold", "image": "assets/images/colors/gold.png"},
+    {"name": "Silver", "hint": "This is Silver", "image": "assets/images/colors/silver.png"},
+    {"name": "Cyan", "hint": "This is Cyan", "image": "assets/images/colors/cyan.png"},
+    {"name": "Magenta", "hint": "This is Magenta", "image": "assets/images/colors/magenta.png"},
 ]
 
 init_animals = {
