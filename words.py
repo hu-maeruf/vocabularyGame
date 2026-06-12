10210a094c677dd8ce8ff5fca2f9bdfa8fe1d6a7
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2026-06-12 22:44:02 +0800

    refactor: add new words

--- words.py
+++ words.py
@@ -2,16 +2,13 @@ init_animals_easy = [
     {"name": "Cat", "hint": "I say meow", "image": "assets/images/animals/cat.png"},
     {"name": "Dog", "hint": "I say woof", "image": "assets/images/animals/dog.png"},
     {"name": "Fish", "hint": "I swim in water", "image": "assets/images/animals/fish.png"},
-    {"name": "Duck", "hint": "I say quack", "image": "assets/images/animals/duck.png"},
     {"name": "Cow", "hint": "I say moo", "image": "assets/images/animals/cow.png"},
     {"name": "Horse", "hint": "I say neigh", "image": "assets/images/animals/horse.png"},
     {"name": "Sheep", "hint": "I say baa", "image": "assets/images/animals/sheep.png"},
     {"name": "Rabbit", "hint": "I love eating carrots", "image": "assets/images/animals/rabbit.png"},
     {"name": "Frog", "hint": "I say ribbit", "image": "assets/images/animals/frog.png"},
-    {"name": "Goat", "hint": "I say maa", "image": "assets/images/animals/goat.png"},
     {"name": "Monkey", "hint": "I live in the forest", "image": "assets/images/animals/monkey.png"},
     {"name": "Lion", "hint": "I am the king of the jungle", "image": "assets/images/animals/lion.png"},
-    {"name": "Tiger", "hint": "I am Tiger", "image": "assets/images/animals/tiger.png"},
 ]
 
 init_animals_diff = [
@@ -28,28 +25,22 @@ init_fruits_easy = [
     {"name": "Orange", "hint": "This is Orange", "image": "assets/images/frut_veg/orange.png"},
     {"name": "Mango", "hint": "This is Mango", "image": "assets/images/frut_veg/mango.png"},
     {"name": "Grapes", "hint": "This is Grapes", "image": "assets/images/frut_veg/grapes.png"},
-    {"name": "Lemon", "hint": "This is Lemon", "image": "assets/images/frut_veg/lemon.png"},
-    {"name": "Watermelon", "hint": "This is Watermelon", "image": "assets/images/frut_veg/watermelon.png"},
-    {"name": "Strawberry", "hint": "This is Strawberry", "image": "assets/images/frut_veg/strawberry.png"},
 ]
 
 init_fruits_diff = [
     {"name": "Pineapple", "hint": "This is Pineapple", "image": "assets/images/frut_veg/pineapple.png"},
-    {"name": "Cherry", "hint": "This is Cherry", "image": "assets/images/frut_veg/cherry.png"},
-    {"name": "Papaya", "hint": "This is Papaya", "image": "assets/images/frut_veg/papaya.png"},
-    {"name": "Blueberry", "hint": "This is Blueberry", "image": "assets/images/frut_veg/blueberry.png"},
+    {"name": "Strawberry", "hint": "This is Strawberry", "image": "assets/images/frut_veg/strawberry.png"},
     {"name": "Avocado", "hint": "This is Avocado", "image": "assets/images/frut_veg/avocado.png"},
     {"name": "Coconut", "hint": "This is Coconut", "image": "assets/images/frut_veg/coconut.png"},
+    {"name": "Watermelon", "hint": "This is Watermelon", "image": "assets/images/frut_veg/watermelon.png"},
 ]
 
 init_veg_easy = [
     {"name": "Carrot", "hint": "This is Carrot", "image": "assets/images/frut_veg/carrot.png"},
-    {"name": "Corn", "hint": "This is Corn", "image": "assets/images/frut_veg/corn.png"},
     {"name": "Potato", "hint": "This is Potato", "image": "assets/images/frut_veg/potato.png"},
     {"name": "Tomato", "hint": "This is Tomato", "image": "assets/images/frut_veg/tomato.png"},
     {"name": "Onion", "hint": "This is Onion", "image": "assets/images/frut_veg/onion.png"},
     {"name": "Cabbage", "hint": "This is Cabbage", "image": "assets/images/frut_veg/cabbage.png"},
-    {"name": "Pepper", "hint": "This is Pepper", "image": "assets/images/frut_veg/pepper.png"},
 ]
 
 init_veg_diff = [
@@ -57,6 +48,7 @@ init_veg_diff = [
     {"name": "Cucumber", "hint": "This is Cucumber", "image": "assets/images/frut_veg/cucumber.png"},
     {"name": "Pumpkin", "hint": "This is Pumpkin", "image": "assets/images/frut_veg/pumpkin.png"},
     {"name": "Ginger", "hint": "This is Ginger", "image": "assets/images/frut_veg/ginger.png"},
+    {"name": "Corn", "hint": "This is Corn", "image": "assets/images/frut_veg/corn.png"},
 ]
 
 init_colors_easy = [
