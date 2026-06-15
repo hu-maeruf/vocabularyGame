449709dc7f14e47965dd89e63753947a66b2b0a6
hu-maeruf <179197304+hu-maeruf@users.noreply.github.com>
2026-06-15 23:29:55 +0800

    refactor: make Button class minimal and reusable

--- components/button.py
+++ components/button.py
@@ -1,22 +1,13 @@
 import pygame
 
 class Button:
-    def __init__(self, position, size, color, text, font_size):
-        self.position = position
-        self.size = size
+    def __init__(self, position, size, color, border_radius=0):
         self.color = color
-        self.text = text
-        self.font_size = font_size
-        self.rect = pygame.Rect((0,0), self.size)
-        self.font = pygame.font.SysFont("Arial", self.font_size)
-        self.rect.center = self.position
-        self.text_surface = self.font.render(self.text, True, (0, 0, 255))
-        self.text_rect = self.text_surface.get_rect()
+        self.rect = pygame.Rect(position, size)
+        self.border_radius = border_radius
 
     def draw(self, screen):
-        self.text_rect.center = self.rect.center
-        pygame.draw.rect(screen, self.color, self.rect)
-        screen.blit(self.text_surface, self.text_rect)
+        pygame.draw.rect(screen, self.color, self.rect, border_radius=self.border_radius)
 
     def is_clicked(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
